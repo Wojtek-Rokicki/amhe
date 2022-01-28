c7db8b80ca244291cde7cadc90662614f3f76497
UlaTwo <44262447+UlaTwo@users.noreply.github.com>
2022-01-28 22:29:34 +0100

    Update final_result_test.sh

--- final_result_test.sh
+++ final_result_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "start algorithm run"
 
-for p in $(seq 1 1 5)
+for p in $(seq 1 1 10)
 do
-    echo "loop: $i "
+    echo "loop: $p "
     python gym_ga.py -s proportional -k averaging -c 0.75 -m 0.75 -n 2 -p 50
 done
