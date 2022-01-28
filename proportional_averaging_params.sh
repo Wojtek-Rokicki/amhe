c7a8ed49c5c0801ddafcd4007b314390fdacf651
UlaTwo <44262447+UlaTwo@users.noreply.github.com>
2022-01-28 22:29:11 +0100

    change

--- proportional_averaging_params.sh
+++ proportional_averaging_params.sh
@@ -2,7 +2,7 @@
 echo "start algorithm experiments - selection and crossover rates"
 
 
-for i in $(seq 1 1 4)
+for i in $(seq 1 1 5)
 do
 
     echo "loop: $i  -c 0.75 -m 0.25 "
@@ -17,8 +17,8 @@ do
     echo "loop: $i  -c 0.25 -m 0.75 "
     python gym_ga.py -s proportional -k averaging -c 0.25 -m 0.75
 
-    # echo "loop: $i  -c 0.25 -m 0.25 "
-    # python gym_ga.py -s proportional -k averaging -c 0.25 -m 0.25
+    echo "loop: $i  -c 0.25 -m 0.25 "
+    python gym_ga.py -s proportional -k averaging -c 0.25 -m 0.25
 
     echo "loop: $i  -c 0.5 -m 0.25  "
 	python gym_ga.py -s proportional -k averaging -c 0.5 -m 0.25
@@ -26,8 +26,11 @@ do
     echo "loop: $i  -c 0.5 -m 0.75  "
     python gym_ga.py -s proportional -k averaging -c 0.5 -m 0.75
 
-    # echo "loop: $i  -c 0.25 -m 0.5  "
-    # python gym_ga.py -s proportional -k averaging -c 0.25 -m 0.5
+    echo "loop: $i  -c 0.25 -m 0.5  "
+    python gym_ga.py -s proportional -k averaging -c 0.25 -m 0.5
+    
+    echo "loop: $i  -c 0.5 -m 0.5  "
+    python gym_ga.py -s proportional -k averaging -c 0.5 -m 0.5
 
 
 done
