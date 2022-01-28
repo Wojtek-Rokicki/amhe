cb34bfa91a61dc73b437bb2b87ec3af630a89ce0
UlaTwo <44262447+UlaTwo@users.noreply.github.com>
2022-01-28 22:30:32 +0100

    Update population_size_experiments.sh

--- population_size_experiments.sh
+++ population_size_experiments.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo "start algorithm experiments"
 
-for p in $(seq 10 10 90)
+for j in $(seq 1 1 5)
 do
-    echo "POPULATION SIZE: $p"
-	python gym_ga.py -p $p -c 0.5 -m 0.5 -v 1 -n 2
-done
+	for p in $(seq 10 10 90)
+	do
+		echo "loop $j POPULATION SIZE: $p"
+		python gym_ga.py -p $p -c 0.75 -m 0.75 -n 2 -s proportional -k averaging
+	done
 
-for p in $(seq 100 100 1000)
-do
-    echo "POPULATION SIZE: $p"
-	python gym_ga.py -p $p -c 0.5 -m 0.5 -v 1 -n 2
+	for p in $(seq 100 100 1000)
+	do
+		echo "loop $j POPULATION SIZE: $p"
+		python gym_ga.py -p $p -c 0.75 -m 0.75 -n 2 -s proportional -k averaging
+	done
 done
